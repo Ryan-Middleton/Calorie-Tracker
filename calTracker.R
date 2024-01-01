6ac2309d77c5d6c76ab47aeae16cf7823ce1a809
Ryan-Middleton <ryanmiddleton04@gmail.com>
2024-01-01 17:20:31 -0500

    functions of formulas, working on checking input

--- calTracker.R
+++ calTracker.R
@@ -1,23 +1,45 @@
-name <- readline(prompt="What is your name?")
 
-age <- as.numeric(readline(prompt="What is your age?"))
+# getNumericOutput <- function(strPrompt){
 
-height <- as.numeric(readline(prompt="What is your height? (In Inches)"))
+#     while(TRUE){
+#         value <- as.numeric(readline(prompt=strPrompt))
+#         print(is.numeric(value))
+#         if(is.numeric(value)) {
+#             if(value >= 0) { return (value) }
+#             else { print("Please enter number greater than 0") }
+#         }
+#         else { print("Please enter numeric value") }
+#         }
+#     return (-1)
 
-weight <- as.numeric(readline(prompt="What is your age? (In Pounds)"))
+# } 
 
 
-BMI = 703 * (weight/(height^2))
+{
+    name <- readline(prompt="What is your name?")
+
+    age <- as.numeric(readline(prompt="What is your age?"))
+
+    height <- as.numeric(readline(prompt="What is your height? (In Inches)"))
+
+    weight <- as.numeric(readline(prompt="What is your age? (In Pounds)"))
+}
+
+BMI <- function(weight, height){
+    return (703 * (weight/(height^2)))
+}
 
 #Basal metabolic rate
 #Mifflin-St Jeor Equation: for men
 #check**
-BMR = (10 * weight) + (6.25 * height) - (5 * age) + 5
+BMR <- function(weight, height, age){
+    return ((10 * weight) + (6.25 * height) - (5 * age) + 5)
+}
 
 print(paste("Hello", name))
 
-print(paste("Your BMI is", BMI))
+print(paste("Your BMI is", BMI(weight, height)))
 
-print(paste("Your basal metabolic rate is", BMR))
+print(paste("Your basal metabolic rate is", BMR(weight, height, age)))
 
 #todo check input type, adjust for metric, add graphic, incorporate into website

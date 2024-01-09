bf7d0140193fd8677b9876c5ee6db7a172acbb2a
Ryan-Middleton <ryanmiddleton04@gmail.com>
2024-01-09 14:20:29 -0500

    graphics

--- calTracker.R
+++ calTracker.R
@@ -1,7 +1,4 @@
-
-install.packages("ggplot2")
-library(ggplot2)
-
+#Handle input type next
 {
     name <- readline(prompt="What is your name?")
 
@@ -9,66 +6,52 @@ library(ggplot2)
 
     height <- as.numeric(readline(prompt="What is your height? (In Inches)"))
 
-    weight <- as.numeric(readline(prompt="What is your weight? (In Pounds)"))
+    lbs <- as.numeric(readline(prompt="What is your weight? (In Pounds)"))
 }
 
-BMI <- function(weight, height){
-    return (703 * (weight/(height^2)))
+BMI <- function(lbs, height){
+    return (703 * (lbs/(height^2)))
 }
 
 #Basal metabolic rate
 #Mifflin-St Jeor Equation: for men
 #check**
-BMR <- function(weight, height, age){
-    return ((10 * weight) + (6.25 * height) - (5 * age) + 5)
+BMR <- function(lbs, height, age){
+    return ((10 * lbs) + (6.25 * height) - (5 * age) + 5)
 }
 
 print(paste("Hello", name))
 
-print(paste("Your BMI is", BMI(weight, height)))
+print(paste("Your BMI is", BMI(lbs, height)))
 
-print(paste("Your basal metabolic rate is", BMR(weight, height, age)))
+print(paste("Your basal metabolic rate is", BMR(lbs, height, age)))
 
 weeks <- c(1:21)
 
-weightArr = c()
-caloriesArr = c()
-bmiArr = c()
+Weight = c()
+Calories = c()
+BodyMassIndex = c()
 
 # Weight loss 0.25 pounds per week
 for (i in weeks){
-    weightArr[i] <- weight-(0.25*i)
-    caloriesArr[i] <- BMR(weight-(0.25*i), height, age+(0.02*i))
-    bmiArr[i] <- BMI(weight-(0.25*i), height)
+    Weight[i] <- lbs-(0.25*i)
+    Calories[i] <- BMR(lbs-(0.25*i), height, age+(0.02*i))
+    BodyMassIndex[i] <- BMI(lbs-(0.25*i), height)
 }
 
 
-plot(weeks, weightArr)
-
 Data_Frame <- data.frame (
-    weightArr,
-    caloriesArr,
-    bmiArr
+    Weight,
+    Calories,
+    BodyMassIndex
 )
 
 print(Data_Frame)
 
+plot.ts(Data_Frame, type="p", col="blue", 
+main="21 Week Plan", 
+sub="This graph shows you your recommended calorie intake and projected BMI and weight over 21 weeks for weight loss.",
+xlab="Week"
+)
 
-
-#plot(ggplot(data = Data_Frame) + geom_point(mapping = aes(x = Value, y = Week)))
-#todo check input type, adjust for metric, add graphic, incorporate into website
-
-# getNumericOutput <- function(strPrompt){
-
-#     while(TRUE){
-#         value <- as.numeric(readline(prompt=strPrompt))
-#         print(is.numeric(value))
-#         if(is.numeric(value)) {
-#             if(value >= 0) { return (value) }
-#             else { print("Please enter number greater than 0") }
-#         }
-#         else { print("Please enter numeric value") }
-#         }
-#     return (-1)
-
-# } 
+#todo check input type, adjust for metric, incorporate into website
